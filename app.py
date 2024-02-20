e522653f5142d4cad29b1715fbfaf3b0f4ab8884
Annalee X. Johnson <annaleexjohnson@gmail.com>
2024-02-20 15:53:23 -0600

    implement microservice

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
+from password_dictionary import login_info
+
+# render index.html
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+def index():
+    return render_template('index.html', password="")
+
+# get user password
+@app.route("/forgot-password", methods= ["POST"])
+def get_pass():
+    username = request.form["username"]
+    unique_id = request.form["unique_id"]
+    print(username, unique_id, type(unique_id))
+
+    for user in login_info:
+        if user["unique_id"] == unique_id and user["username"] == username:
+            user_pass = user['password']
+            return render_template('index.html', password=f'Your password is: "{user_pass}"') # return password
+        
+    return render_template('index.html', password="We couldn't find your password. Try again.")    # handles error
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ password_dictionary.py
@@ -0,0 +1,17 @@
+login_info = [
+    {
+        "username": "user1",
+        "password": "apple",
+        "unique_id": "12"
+    },
+    {
+        "username": "user2",
+        "password": "banana",
+        "unique_id": "34"
+    },
+    {
+        "username": "user3",
+        "password": "pear",
+        "unique_id": "56"
+    }
+]
